7f061f1b2879e3ac3cb93374911d93bc71ce0d3d
OwenGranot <58481800+OwenGranot@users.noreply.github.com>
2022-03-28 15:00:45 +0300

    messaging

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ const peerServer = ExpressPeerServer(server, {
 
 const { isMeetingOnline } = require("./api");
 
+const MAX_MESSAGE_LENGTH = 200;
+
 app.use(cors());
 app.use("/peerjs", peerServer);
 
@@ -26,6 +28,72 @@ app.get("/", (req, res) => {
   res.send("Homeroom server");
 });
 
+// class Participant {
+//   socketId;
+//   uid;
+
+//   constructor(sid, uid) {
+//     this.socketId = sid;
+//     this.uid = uid;
+//   }
+
+//   getSocketId() {
+//     return this.socketId;
+//   }
+//   getId() {
+//     return this.uid;
+//   }
+// }
+
+// class Rooms {
+//   rooms = [];
+//   constructor() {}
+
+//   getRoom(id) {
+//     return this.rooms.find((room) => room.id === id);
+//   }
+//   isRoomExists(id) {
+//     return !!this.getRoom(id);
+//   }
+//   addRoom(id) {
+//     return isRoomExists(id) ? this.getRoom(id) : new Room();
+//   }
+// }
+
+// class Room {
+//   id;
+//   participants = [];
+//   admin;
+//   constructor() {}
+
+//   addParticipant(socketId, userId) {
+//     const participant = new Participant(socketId, userId);
+//     this.participants.push(participant);
+//     return participant;
+//   }
+//   removeParticipantByUserId(uid) {
+//     this.participants = this.participants.filter(
+//       (participant) => participant.uid !== uid
+//     );
+//   }
+
+//   removeParticipantBySocketId(sid) {
+//     this.participants = this.participants.filter(
+//       (participant) => participant.socketId !== sid
+//     );
+//   }
+
+//   isParticipant(uid) {
+//     return !!this.participants.find((participant) => participant.uid === uid);
+//   }
+
+//   getAdmin() {
+//     return this.admin;
+//   }
+// }
+
+// const rooms = new Rooms();
+
 io.on("connection", (socket) => {
   console.log("subscriber connected:", socket.id);
 
@@ -33,10 +101,6 @@ io.on("connection", (socket) => {
     console.log("subscriber disconnected:", socket.id);
   });
   socket.on("join-room", (roomId, userId) => {
-    socket.on("new-stream", () => {
-      console.log(userId, "new stream");
-      socket.broadcast.to(roomId).emit("new-stream", userId);
-    });
     socket.on("ready", () => {
       console.log(socket.id, "declared ready");
 
@@ -49,6 +113,21 @@ io.on("connection", (socket) => {
           socket.on("disconnect", () => {
             console.log(userId, "disconnected from", roomId);
             socket.broadcast.to(roomId).emit("user-disconnected", userId);
+          });
+          socket.on("new-stream", () => {
+            console.log(userId, "new stream");
+            socket.broadcast.to(roomId).emit("new-stream", userId);
+          });
+
+          socket.on("message", (message) => {
+            if (!message || message.length > MAX_MESSAGE_LENGTH) return;
+            console.log(`[${roomId}] (${userId}): ${message}`);
+            socket.broadcast.to(roomId).emit("message", {
+              sender: userId,
+              socketId: socket.id,
+              message: message,
+              time: new Date().toISOString(),
+            });
           });
         })
         .catch((error) => {
