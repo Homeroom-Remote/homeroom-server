b7d55c0b734a8c727f59bf30bbe90f15b18dc3e3
OwenGranot <58481800+OwenGranot@users.noreply.github.com>
2022-05-09 15:05:19 +0300

    finishes gestures

--- rooms/PeerMeeting.js
+++ rooms/PeerMeeting.js
@@ -36,6 +36,9 @@ class PeerMeetingRoom extends Room {
 
     // Success
 
+    this.roomId = meetingId;
+    console.log("opened meeting", this.roomId);
+
     // Register message callbacks
     this.onMessage("chat-message", (client, message) => {
       const senderObject = this.participants.get(client.sessionId);
@@ -82,8 +85,9 @@ class PeerMeetingRoom extends Room {
         name: senderObject.name,
       });
     });
+
     // Open meeting on server
-    this.roomId = meetingId;
+
     this.owner = uid;
     const meetingData = await openMeetingOnServer(meetingId);
 
@@ -92,7 +96,6 @@ class PeerMeetingRoom extends Room {
 
   // Authorize client (before onJoin)
   async onAuth(clieznt, options, request) {
-    console.log(options);
     const selectedName = options.name;
     const userData = await validateToken(options.accessToken);
     if (userData) {
@@ -125,6 +128,7 @@ class PeerMeetingRoom extends Room {
 
   // When client leaves the room
   onLeave(client, consented) {
+    console.log(client.sessionId, "left");
     this.broadcast("leave", { sessionId: client.sessionId });
     if (this.participants.has(client.sessionId)) {
       const uid = this.participants.get(client.sessionId).uid;
@@ -135,7 +139,7 @@ class PeerMeetingRoom extends Room {
 
   // Cleanup, called after no more clients
   async onDispose() {
-    console.log("No more clients, closing room");
+    console.log("No more clients, closing room", this.roomId);
     this.roomId &&
       closeMeetingOnServer(this.roomId)
         .then(() => {})
