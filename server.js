d977374a959413f0fc45d22c05122a16d10be3e4
OwenGranot <58481800+OwenGranot@users.noreply.github.com>
2022-05-25 03:14:20 +0300

    queries room on index page

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const config = require("config");
 const colyseus = require("colyseus");
+const matchMaker = require("colyseus").matchMaker;
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
@@ -13,6 +14,10 @@ const onListen = (port) => (err) => {
 const port = process.env.PORT || config.app.port;
 
 const roomServer = new colyseus.Server({ server });
+app.get("/", async (req, res) => {
+  const rooms = await matchMaker.query({ name: "peer" });
+  res.send(rooms);
+});
 roomServer.define("peer", PeerMeetingRoom);
 
 server.listen(port, onListen(port));
